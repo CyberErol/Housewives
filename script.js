04c4df5aa3581c38e5c46081a07888a29f3d06e2
Martin Trumann <martin.trumann@gmail.com>
2020-03-04 14:12:06 +0200

    added everything

--- script.js
+++ script.js
@@ -1,16 +1,87 @@
 const select = document.querySelector("#select")
+const tot = document.querySelector("#tot")
+const num = document.querySelector("#num")
+const form = document.querySelector("#form")
+const itemList = document.querySelector("#itemList")
+const dateElm = document.querySelector("#date")
 
-url = "http://andmebaas.stat.ee/sdmx-json/data/PA624/1.1+2+3+4+5+6+7+8+9+10+11+12+13+14+15+16+17+18+19+20.1/all?startTime=2010&endTime=2014&dimensionAtObservation=allDimensions"
-fetch(url).then(raw => {return raw.json()}).then(data => {
-	let fields = data.structure.dimensions.observation[1].values
-	fields.forEach(i => {
+fetch("./names.json").then(raw => {return raw.json()}).then(data => {
+	for(item in data){
 		let opt = document.createElement("option")
-		opt.innerText = i.name;
+		opt.innerText = data[item];
+		opt.value = item;
 		select.appendChild(opt)
-		console.log(i.name)
-	})
-	obs = data.dataSets[0].observations
-	for(item in obs){
-		let arrI = item.split(":")
 	}
-})
+
+	form.addEventListener("submit", e => {
+		e.preventDefault()
+		let field = select.children[select.selectedIndex]
+		let date = dateElm.value;
+		let hour = num.value
+		if(hour > 0){
+			let url = "http://andmebaas.stat.ee/sdmx-json/data/PA633/" + field.value + ".3.1/all?startTime=2014&endTime=2014&dimensionAtObservation=allDimensions"
+			fetch(url).then(raw => {return raw.json()}).then(data => {
+				let sum = data.dataSets[0].observations["0:0:0:0"][0] * hour;
+				new Entry(field.innerText, hour, date, sum)
+			})
+		}else{
+			console.log("tunde ei saa olla vähem kui null")
+		}
+	})
+});
+
+function Entry(name, hour, date, sum){
+	this.name = name;
+	this.hour = parseInt(hour);
+	this.sum = sum;
+	this.date = date;
+
+	this.rm = function(){
+		calc.ents.splice(calc.ents.indexOf(this), 1)
+		this.elm.remove();
+		calc.tot();
+	}
+
+	this.elm = document.createElement("div")
+
+	var nameElm = document.createElement("SPAN")
+	nameElm.innerText = name
+	this.elm.appendChild(nameElm)
+
+	var hourElm = document.createElement("SPAN")
+	hourElm.innerText = this.hour;
+	this.elm.appendChild(hourElm)
+
+	var sumElm = document.createElement("SPAN")
+	sumElm.innerText = (Math.round((this.sum + Number.EPSILON) * 100) / 100) + "€";
+	this.elm.appendChild(sumElm)
+
+	var btnElm = document.createElement("button")
+	btnElm.innerText = "X"
+	this.elm.appendChild(btnElm)
+
+	btnElm.addEventListener("click", e => {
+		this.rm();
+	})
+
+	calc.ents.push(this);
+	calc.add(this.elm);
+
+	calc.tot();
+}
+
+var calc = {
+	ents: [],
+	total: 0,
+	tot: function(){
+		var res = 0;
+		this.ents.forEach((item, i) => {
+			res += item.sum;
+		})
+		tot.innerText = (Math.round((res + Number.EPSILON) * 100) / 100);
+
+	},
+	add: function(elm){
+		itemList.appendChild(elm)
+	}
+}
